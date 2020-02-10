75218af2b447ed1304f8732f1bfc3c0cbc1f9b75
Darion Suggs <dariontsuggs@icloud.com>
2020-02-10 12:40:20 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,85 @@
-import React, { Component } from "react";
-import { Card, Icon } from "semantic-ui-react";
+// import React, { Component } from "react";
+// import { Card, Icon, } from "semantic-ui-react";
 
+// import "./App.css";
+
+// const gitHubAPI = "https://api.github.com/users/darionsuggs";
+
+// class App extends Component {
+//   state = {
+//     user: {},
+//     active: false
+//   };
+
+//   handleToggle = event => {
+//     if (this.state.active === false) {
+//       fetch(gitHubAPI)
+//         .then(response => response.json())
+//         .then(responseBody => {
+//           console.log(responseBody);
+//           this.setState({ user: responseBody, active: true });
+//         });
+//     } else {
+//       this.setState({ active: false });
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <button class="ui button" onClick={this.handleToggle}>
+//           Toggle User
+//         </button>
+//         <Card>
+//           <Card.Content>
+//             {this.state.active && (
+//               <div>
+//                 <img
+//                   className="img"
+//                   src={this.state.user.avatar_url}
+//                   alt="My Profile"
+//                 />
+//                 <Card.Description>{this.state.user.name}</Card.Description>
+//                 <Card.Description>
+//                   Followers: {this.state.user.followers}
+//                   <hr></hr>
+//                 </Card.Description>
+//                 <Card.Description>
+//                   <Icon name="location arrow" /> Location:{" "}
+//                   {this.state.user.location}
+//                 </Card.Description>
+//               </div>
+//             )}
+//           </Card.Content>
+//         </Card>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import { Card, Icon, Image } from "semantic-ui-react";
 import "./App.css";
 
+// Variables Section
 const gitHubAPI = "https://api.github.com/users/darionsuggs";
 
+// Class component
 class App extends Component {
   state = {
     user: {},
     active: false
   };
-
+  // General method on App
   handleToggle = event => {
     if (this.state.active === false) {
       fetch(gitHubAPI)
         .then(response => response.json())
         .then(responseBody => {
-          console.log(responseBody);
+          // console.log(responseBody);
           this.setState({ user: responseBody, active: true });
         });
     } else {
@@ -24,35 +87,37 @@ class App extends Component {
     }
   };
 
+  // Lifecycle method on App
   render() {
     return (
-      <>
-        <button className="ui button" onClick={this.handleToggle}>
-          Toggle User
-        </button>
-        <Card>
-          <Card.Content>
-            {this.state.active && (
-              <div>
-                <img
-                  className="img"
-                  src={this.state.user.avatar_url}
-                  alt="My Profile"
-                />
-                <Card.Description>{this.state.user.name}</Card.Description>
+      <React.Fragment>
+        <Button onClick={this.handleToggle}>Toggle User</Button>
+        {this.state.active && (
+          <React.Fragment>
+            <Card>
+              <Image
+                src={this.state.user.avatar_url}
+                alt="User Profile Pic"
+                wrapped
+                ui={false}
+              />
+              <Card.Content>
+                <Card.Header>{this.state.user.name}</Card.Header>
                 <Card.Description>
-                  Followers: {this.state.user.followers}
-                  <hr></hr>
+                  Lives in: {this.state.user.location}
                 </Card.Description>
                 <Card.Description>
-                  <Icon name="location arrow" /> Location:{" "}
-                  {this.state.user.location}
+                  Name: {this.state.user.name}
                 </Card.Description>
-              </div>
-            )}
-          </Card.Content>
-        </Card>
-      </>
+              </Card.Content>
+              <Card.Content extra>
+                <Icon name="user" />
+                Followers: {this.state.user.followers}
+              </Card.Content>
+            </Card>
+          </React.Fragment>
+        )}
+      </React.Fragment>
     );
   }
 }
